72adf0d39b722bf7699fdaa7e16f96f39b37a7e8
visualhub23 <96225344+visualhub23@users.noreply.github.com>
2023-03-05 20:17:32 +0700

    Create 121140041_GBintangAndromeda_Prak1_1.py

---
+++ tugas1/121140041_GBintangAndromeda_Prak1_1.py
@@ -0,0 +1,8 @@
+# inputan panjang
+n = int(input("inputtan panjang n : "))
+
+# menggunakan double for loop untuk menghasilkan bentuk persegi
+for i in range(n):
+    for j in range(n):
+        print("*",end="")
+    print("")
