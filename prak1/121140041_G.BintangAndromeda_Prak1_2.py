6244fb5f9c8923ee949448a2483780b81d9b136b
visualhub23 <96225344+visualhub23@users.noreply.github.com>
2023-03-05 20:06:18 +0700

    Update 121140041_G.BintangAndromeda_Prak1_2.py

--- prak1/121140041_G.BintangAndromeda_Prak1_2.py
+++ prak1/121140041_G.BintangAndromeda_Prak1_2.py
@@ -1,11 +1,13 @@
+# data password benar disimpan dalam variabel
 username = "informatika"
 password = "12345678"
 
-
+# percobaan memasukkan password jika lebih dari 3 kali diblokir
 for i in range(4):
+    # inputan di casting ke tipe data string
     inputUsername = str(input("Enter your username : "))
     inputPassword = str(input("Enter your password : "))
-    
+    # percabangan logic untuk validasi login
     if inputUsername == username and inputPassword == password:
         print("Username anda : ", inputUsername)
         print("Password anda : ", inputPassword)
